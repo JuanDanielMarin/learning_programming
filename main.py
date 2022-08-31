924978ea48567326f19f9ef90ff16a9ccaecbb60
JuanDataAnalyst <juandataanalyst@gmail.com>
2022-08-31 09:45:33 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+saludo = 'Hola'
+nombre = 'Juan'
+
+print(f'{saludo} {nombre}')
+
+#Funciones
+salida = input('Ingrese su nombre:')
+
+num1 = int(input('Ingrese el numero 1:'))
+num2 = int(input('Ingrese el numero 2:'))
+print(num1 + num2)
