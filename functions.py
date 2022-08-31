b464a13ca392c4e79ec11243145b3c95cd51bb29
JuanDataAnalyst <juandataanalyst@gmail.com>
2022-08-31 17:28:29 -0400

    First commit

---
+++ functions.py
@@ -0,0 +1,18 @@
+import random
+
+def get_stats():
+    dados = [random.randint(1,6) for i in range(4)]
+    dados.sort()
+    max_dados = dados[1:]
+    return sum(max_dados)
+
+stats = {
+    'str': get_stats(),
+    'des': get_stats(),
+    'int': get_stats(),
+    'sab': get_stats(),
+    'con': get_stats()
+}
+
+print(stats)
+
