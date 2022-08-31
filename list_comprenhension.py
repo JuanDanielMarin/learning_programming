e48a6dfffd2941b0b602bb0183675d19714dc782
JuanDataAnalyst <juandataanalyst@gmail.com>
2022-08-31 15:06:23 -0400

    First commit

---
+++ list_comprenhension.py
@@ -0,0 +1,20 @@
+import random
+
+alumnos=[]
+for i in range(30):
+    notas = [random.randint(1,10) for i in range(3)]
+    alumnos.append(notas)
+else:
+    print(f'Las notas se han cargado exitosamente')
+
+promedios = []
+for i in range(30):
+    notas = alumnos[i]
+    suma = 0
+    for nota in notas:
+        suma = nota + suma
+        prom = round((suma / 3),2)
+        promedios.append(prom)
+
+print(promedios)
+ 
