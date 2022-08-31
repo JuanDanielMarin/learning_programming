f0c8951df15b900a87633ccbcb2c56e0698fe77d
JuanDataAnalyst <juandataanalyst@gmail.com>
2022-08-31 14:06:46 -0400

    First Commit

---
+++ random_game.py
@@ -0,0 +1,71 @@
+'''
+Debemos tirar 4 dados y quedarnos con la suma de los 3 mejores
+'''
+
+import random
+
+dado1 = random.randint(1,6)
+dado2 = random.randint(1,6)
+dado3 = random.randint(1,6)
+dado4 = random.randint(1,6)
+
+suma = dado1 + dado2 + dado3 + dado4
+menor = min(dado1, dado2, dado3, dado4)
+result = suma - menor
+
+print(f'Fuerza {result}')
+
+dado1 = random.randint(1,6)
+dado2 = random.randint(1,6)
+dado3 = random.randint(1,6)
+dado4 = random.randint(1,6)
+
+suma = dado1 + dado2 + dado3 + dado4
+menor = min(dado1, dado2, dado3, dado4)
+result = suma - menor
+
+print(f'Destreza {result}')
+
+dado1 = random.randint(1,6)
+dado2 = random.randint(1,6)
+dado3 = random.randint(1,6)
+dado4 = random.randint(1,6)
+
+suma = dado1 + dado2 + dado3 + dado4
+menor = min(dado1, dado2, dado3, dado4)
+result = suma - menor
+
+print(f'Constitución {result}')
+
+dado1 = random.randint(1,6)
+dado2 = random.randint(1,6)
+dado3 = random.randint(1,6)
+dado4 = random.randint(1,6)
+
+suma = dado1 + dado2 + dado3 + dado4
+menor = min(dado1, dado2, dado3, dado4)
+result = suma - menor
+
+print(f'Inteligencia {result}')
+
+dado1 = random.randint(1,6)
+dado2 = random.randint(1,6)
+dado3 = random.randint(1,6)
+dado4 = random.randint(1,6)
+
+suma = dado1 + dado2 + dado3 + dado4
+menor = min(dado1, dado2, dado3, dado4)
+result = suma - menor
+
+print(f'Sabiduria {result}')
+
+dado1 = random.randint(1,6)
+dado2 = random.randint(1,6)
+dado3 = random.randint(1,6)
+dado4 = random.randint(1,6)
+
+suma = dado1 + dado2 + dado3 + dado4
+menor = min(dado1, dado2, dado3, dado4)
+result = suma - menor
+
+print(f'Carisma {result}')
